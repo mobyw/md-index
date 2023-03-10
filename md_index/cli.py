f6c98aa11f2f5dac36c06496620f15731a3ef9c7
mobyw <44370805+mobyw@users.noreply.github.com>
2023-03-10 20:20:14 +0800

    📦 NEW: improve code

--- md_index/cli.py
+++ md_index/cli.py
@@ -6,19 +6,37 @@ from md_index.index import generate_index
 
 
 @click.command()
-@click.argument("directory", type=click.Path(exists=True), default=".")
+@click.option(
+    "-i",
+    "--input-dir",
+    type=click.Path(exists=True),
+    default=".",
+    help="Input directory.",
+)
 @click.option(
     "-o",
-    "--output",
+    "--output-dir",
     type=click.Path(file_okay=False, writable=True),
-    default=".temp",
+    default="docs",
     help="Output directory.",
 )
-def generate(directory: str, output: str):
-    output_dir = Path(output)
-    output_dir.mkdir(parents=True, exist_ok=True)
-    generate_index(Path(directory), output_dir)
-    click.echo(f"Markdown index generated for {directory} in {output_dir}")
+@click.option(
+    "-d",
+    "--depth",
+    type=click.INT,
+    default=1,
+    help="Depth of the file tree to generate, 1 or 2.",
+)
+@click.option(
+    "-u",
+    "--url-prefix",
+    type=click.STRING,
+    default="",
+    help="Prefix for the URLs in the generated index.",
+)
+def generate(input_dir: str, output_dir: str, depth: int, url_prefix: str):
+    generate_index(Path(input_dir), Path(output_dir), depth, url_prefix)
+    click.echo(f'Markdown index generated for "{input_dir}"" in "{output_dir}"')
 
 
 if __name__ == "__main__":
